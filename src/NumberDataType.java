b8fd29c8552fc298bef8babee0530e424ffa453c
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-04-23 23:59:54 +0700

    dataTypeNumber

---
+++ src/NumberDataType.java
@@ -0,0 +1,21 @@
+public class NumberDataType {
+
+    public static void main(String[] args) {
+
+        // Basic numberDataType
+        byte thisByte = 100;
+        short thisShort = 1000;
+        int thisInt = 10000000;
+        long thisLong = 100000000;
+        long thisLong2 = 100000000L;
+
+        // Floating point number
+        float thisFloat = 10.10F;
+        double thisDouble = 10.10;
+
+        // Literals
+        int decimalInt = 25;
+        int hexInt = 0x3
+    }
+
+}
