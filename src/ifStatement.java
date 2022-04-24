83498fa8e9f2513a3820cf6d74b92a22a76c5990
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-04-24 15:58:20 +0700

    complex if statement

--- src/ifStatement.java
+++ src/ifStatement.java
@@ -1,5 +1,5 @@
 public class ifStatement {
-    public static void main(String[] args) {
+    public static void main1(String[] args) {
         var nilai = 75;
         var absen = 90;
 
@@ -9,5 +9,22 @@ public class ifStatement {
             System.out.println("You not graduate");
         }
 
+    }
+
+    public static void main(String[] args) {
+        var nilai = 80;
+        var absen = 90;
+
+        if (nilai >= 80 && absen >= 80 ) {
+            System.out.println("Your grade A");
+        } else if (nilai >= 70 && absen >= 70) {
+            System.out.println("Your grade B");
+        } else if (nilai >= 60 && absen >= 65) {
+            System.out.println("Your grade C");
+        } else if (nilai >= 50 && absen >= 60) {
+            System.out.println("Your grade D");
+        } else {
+            System.out.println("Your grade E");
+        }
     }
 }
