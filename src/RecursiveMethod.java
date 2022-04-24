59a4d72bd3cb701d9c84ddb3051858bbed7604d7
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-04-24 22:23:44 +0700

    learn recursice to made stack overflow

--- src/RecursiveMethod.java
+++ src/RecursiveMethod.java
@@ -1,14 +1,37 @@
 public class RecursiveMethod {
     public static void main(String[] args) {
-        System.out.println(factorial(10));
+        System.out.println(factorial(5));
+        System.out.println("\nThis are methode recursive");
+        System.out.println(factorialRecursive(5));
+        System.out.println("\nThis are method recursive to be stack overflow");
+        loop(1);
     }
 
+    // Factorial with ussually Loop
     static int factorial(int value) {
-        // Factorial Loop
         var result = 1;
         for (int i = 1; i <= value; i++) {
             result *= i;
         }
         return result;
+    }
+
+    // Factorial use recursive method
+    static int factorialRecursive(int value) {
+        if (value == 1) {
+            return 1;
+        } else {
+            return value * factorialRecursive(value - 1);
+        }
+    }
+
+    // Made methode to be Error StackOverflow when we have to recursive
+    static void loop(int value) {
+        if (value == 0) {
+            System.out.println("Done");
+        } else {
+            System.out.println("Loop- " + value);
+            loop(value + 1);
+        }
     }
 }
