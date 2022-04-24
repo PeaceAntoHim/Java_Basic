fa931a3c578fd0e21106c2b1497fc0bb83152b17
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-04-24 15:50:30 +0700

    learn if statement

--- src/ExpressionState.java
+++ src/ExpressionState.java
@@ -1,3 +1,20 @@
 public class ExpressionState {
+    public static void main(String[] args) {
+        // This are expression
+        int value;
+        value = 10;
+
+        System.out.println(value = 100);
+
+        // Code Statement
+            // Assignment statement
+            double aValue = 8933.234;
+            // Method invocation statement
+            aValue++;
+            // Method invocation statement
+        System.out.println("Hello World");
+        // Objext creation statement
+//        Date date = new Date();
+    }
 
 }
---
+++ src/ifStatement.java
@@ -0,0 +1,13 @@
+public class ifStatement {
+    public static void main(String[] args) {
+        var nilai = 75;
+        var absen = 90;
+
+        if (nilai >= 75 && absen >= 75) {
+            System.out.println("You graduate");
+        } else {
+            System.out.println("You not graduate");
+        }
+
+    }
+}
