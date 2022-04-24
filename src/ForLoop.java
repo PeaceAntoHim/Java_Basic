346555e9d555833d56483fc2f7b20a4079c6aef3
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-04-24 16:42:09 +0700

    learn for loop

---
+++ src/ForLoop.java
@@ -0,0 +1,28 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        // For without any condition
+         for(;;) {
+             System.out.println("Loop Forever");
+         }
+        // For with condition
+        var counter1 = 1;
+        for (; counter1 <= 10 ;) {
+            System.out.println("Loop " + counter1);
+
+            counter1++;
+        }
+
+        System.out.println(counter1);
+
+        // For with init statement
+        for (var counter = 1; counter <= 10;) {
+            System.out.println("Loop for-" + counter);
+            counter++;
+        }
+        // For with Post Statement
+        for (var counter = 1; counter <= 10; counter++) {
+            System.out.println("Loop " + counter);
+        }
+
+    }
+}
---
+++ src/TernaryOperator.java
@@ -0,0 +1,7 @@
+public class TernaryOperator {
+    public static void main(String[] args) {
+        var nilai = 75;
+        String ucapan = nilai >= 75 ? "Congratulation you was graduate" : "Try for new year";
+        System.out.println(ucapan);
+    }
+}
