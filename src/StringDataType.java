9634d84b06f8d74c8e9001dff4be117f8690da1d
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-04-24 12:26:34 +0700

    added new string leason

--- src/StringDataType.java
+++ src/StringDataType.java
@@ -25,6 +25,22 @@ public class StringDataType {
         System.out.println(name);
         System.out.println(age);
         System.out.println(addreas);
-    }
 
+        // Contoh Ketiga dengan kata kunci var
+
+        var firstName1 = "Frans";
+        var lastName1 = "Sebastian";
+        var age1 = 18;
+        var addreas1 = "Jln. Pluit Permai";
+
+        System.out.println(firstName1);
+        System.out.println(lastName1);
+        System.out.println(age1);
+        System.out.println(addreas1);
+
+        // Contoh ketiga dengan kata kunci final
+        final String name2 = "Frans Sebastian";
+        // Final variable like const at js we can to added new data.
+        System.out.println(name2);
+    }
 }
